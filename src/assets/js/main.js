e1f17453a6eb14630a30f8545f498af3efc05d6e
CH,KIM <kchstar@outlook.com>
2024-05-09 16:46:32 +0900

    🤣레이아웃완성

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,7 @@
-// import { menu } from "./menu.js";
-// import { port } from "./port.js";
-// import { link } from "./link.js";
-// import { smooth } from "./smooth.js";
-
+import { menu} from "./menu.js";
+import { port } from "./port.js";
+import { link } from "./link.js";
+import { smooth } from "./smooth.js";
 window.addEventListener("load", function(){
     menu();
     port();
